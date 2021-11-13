501d6753067b731b67c02a100603784a786d9f83
Logan Falkenberg <loganf0101@gmail.com>
2021-11-13 17:38:15 -0600

    Copied code from previous project to insert prototype methods into class

--- linkedlist.h
+++ linkedlist.h
@@ -5,14 +5,38 @@
 #ifndef SIMPLE_LINKED_LIST_LINKEDLIST_H
 #define SIMPLE_LINKED_LIST_LINKEDLIST_H
 
+#include "data.h"
+#include <iostream>
+
 class LinkedList{
 
-    public:
+public:
 
+    // Constructor/Destructor
+    LinkedList();
+    ~LinkedList();
 
-    private:
-    
+    //Methods
+    bool addNode(int, string*);
+    bool deleteNode(int);
+    bool getNode(int, Data*);
+    void printList(bool = false);
+    int getCount();
+    bool clearList();
+    bool exists(int);
 
-};
+private:
+
+    // Pointer to the first node
+    Node *head;
+
+    // Private methods
+    void initializeNode(int *, string*, Node*);
+    bool checkOperation(Node *, Node *, Node *, int *);
+    void insertHead(Node *);
+    void insertMiddle(Node *, Node *, Node *);
+    void insertTail(Node *, Node *);
+
+}; // End of class LinkedList
 
 #endif //SIMPLE_LINKED_LIST_LINKEDLIST_H
